aa9b2c80d88c01cb46c23a9bd11727fd802b37e4
mfindler <mfindler@cs.wright.edu>
2019-04-10 20:13:13 -0500

    Add project files.

---
+++ Assets/Character Selector/Scripts/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+    public Material[] furColor;
+    public Material[] eyeColor;
+    public GameObject body;
+    public GameObject rightEye;
+    public GameObject leftEye;
+
+    Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+    
+        int colorIndex = (int)(Random.value * 3);
+
+        RenderCube(body, colorIndex, furColor);
+
+        colorIndex = (int)(Random.value * 3);
+        RenderCube(rightEye, colorIndex, eyeColor);
+        RenderCube(leftEye, colorIndex, eyeColor);
+
+    }
+
+    private void RenderCube(GameObject obj,  int colorIndex, Material[] color)
+    {
+        rend = obj.GetComponent<Renderer>();
+        rend.enabled = true;
+
+        rend.sharedMaterial = color[colorIndex];
+    }
+}
---
+++ Assets/Character Selector/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject myCat;
+
+    public void Spawn(Vector3 position)
+    {
+        Instantiate(myCat).transform.position = position;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Make Z position the Z position of prefab object.
+            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, myCat.transform.position.z);
+
+            Spawn(adjustZ);
+        }
+    }
+}
